e9ef956be81311957853ee4de72b810497b379eb
valbmsturu <48201282+valbmsturu@users.noreply.github.com>
2021-04-07 10:37:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ cp dhcp-pools-discovery.sh /etc/zabbix/
 
 cp dhcp-pools-shared-network.sh /etc/zabbix/
 
-echo 'UserParameter=dhcp.pools.discovery,/etc/zabbix/dhcp-pools-discovery.sh' >> /etc/zabbix/zabbix_agentd.conf
+echo 'UserParameter=dhcp.pools.discovery,/etc/zabbix/dhcp-pools-discovery.sh' > /etc/zabbix/zabbix_agentd*.d/dhcp_stat.conf
 
-echo 'UserParameter=dhcp.pools.shared-network[*],/etc/zabbix/dhcp-pools-shared-network.sh $1 $2' >> /etc/zabbix/zabbix_agentd.conf 
+echo 'UserParameter=dhcp.pools.shared-network[*],/etc/zabbix/dhcp-pools-shared-network.sh $1 $2' >> /etc/zabbix/zabbix_agentd*.d/dhcp_stat.conf 
 
 service zabbix-agent restart
 
